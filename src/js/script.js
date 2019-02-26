9598c87471f5476839db654fbd451e287e11d1f3
gabrielIV <gabrielkamau9@gmail.com>
2019-02-26 10:20:57 +0300

    added full covering loader

--- src/js/script.js
+++ src/js/script.js
@@ -3,3 +3,10 @@ var tl = new TimelineMax({ repeat: -1 });
 tl.to("#logo-img", 3, { rotation: 360, ease: Power4.easeInOut })
   .staggerFrom(".loader-tbg", 1, { opacity: 0 }, 0.2, "-=2")
   .staggerTo(".loader-tbg", 1, { opacity: 0 }, 0.2);
+
+setTimeout(() => {
+  var tl2 = new TimelineMax();
+  tl2.to(".loader-curtain", 1, { opacity: 0 }).set(".loader-curtain", {
+    display: "none"
+  });
+}, 6000);
